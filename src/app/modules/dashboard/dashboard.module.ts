fdc0d04e083473e7d5001565135c0168a04eefd8
Chanaka Guansekara <chanaka.gunasekara@blackswan-technologies.com>
2022-06-26 19:12:32 +0530

    dashboard improvements

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { AddRecordComponent } from './components/add-record/add-record.component';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [
@@ -9,7 +10,8 @@ import { AddRecordComponent } from './components/add-record/add-record.component
     AddRecordComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatProgressSpinnerModule
   ],
   exports: [
     DashboardComponent,
