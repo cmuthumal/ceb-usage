3f296b000e32f13f681f046e036c4e8404836606
Chanaka Guansekara <chanaka.gunasekara@blackswan-technologies.com>
2022-06-26 17:00:37 +0530

    dummy login function

--- src/app/modules/core/components/login/login.component.ts
+++ src/app/modules/core/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,83 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  public loginInvalid: boolean = false;
+  private formSubmitAttempt: boolean = false;
+  private returnUrl: string = '';
+  authDataLoaded = true;
 
-  ngOnInit(): void {
+  constructor(
+    private router: Router,
+    private fb: FormBuilder
+  ) {
+    this.form = this.fb.group({
+      username: ['sisira', Validators.required],
+      password: ['sisira1', Validators.required]
+    });
   }
 
+  ngOnInit(): void {
+    sessionStorage.clear();
+  }
+
+  async onSubmit() {
+    this.authDataLoaded = false;
+    this.loginInvalid = false;
+    this.formSubmitAttempt = false;
+
+    if (this.form.valid) {
+      try {
+        let data = {
+          username: this.getFormValue('username').value,
+          password: this.getFormValue('password').value
+        };
+
+        console.log(data);
+        
+
+        if (data.username == 'sisira' && data.password == 'sisira1') {
+          this.authDataLoaded = true;
+          this.signin({
+            firstName: "Sisira",
+            lastName: "Gunasekara"
+          });
+          this.router.navigate(['/dashboard']);
+        } else {
+          this.authDataLoaded = true;
+          this.loginInvalid = true;
+        }
+
+        // await this.authService.login(data).subscribe(res => {
+        //   if (res && res.email) {
+        //     this.authDataLoaded = true;
+        //     this.signin(res);
+        //   } else {
+        //     this.authDataLoaded = true;
+        //     this.loginInvalid = true;
+        //   }
+        // }, error => {
+        //   if (error.status == 403) {
+        //     this.authDataLoaded = true;
+        //     this.loginInvalid = true;
+        //   }
+        // });
+      } catch (err) {
+        this.authDataLoaded = true;
+        this.loginInvalid = true;
+      }
+    } else {
+      this.formSubmitAttempt = true;
+      this.authDataLoaded = true;
+    }
+  }
+
+  getFormValue(key: string) {
+    return this.form.get(key) as FormControl;
+  }
+
+  signin(user: any) {
+    sessionStorage.setItem('user', JSON.stringify(user));
+    sessionStorage.setItem('token', '3ghw7-tr7935-dsvb3-er03f-pklm2');
+  }
 }
--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -1,5 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
 import { HeaderComponent } from './components/header/header.component';
 import { LoginComponent } from './components/login/login.component';
 
@@ -9,7 +20,17 @@ import { LoginComponent } from './components/login/login.component';
     LoginComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatSelectModule,
+    MatMenuModule,
+    MatProgressSpinnerModule
   ],
   exports: [
     HeaderComponent,
